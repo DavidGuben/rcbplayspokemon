55216db82cef80d01a99117cc1469eca717e9b6e
Mike Magnani <mikemagnani5@gmail.com>
2016-08-13 12:11:52 -0400

    added db

--- redis.js
+++ redis.js
@@ -1,8 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars');
+var sequelize = require('sequelize');
 
-var redis = require('redis');
-var uri = process.env.WEPLAY_REDIS_URI || 'localhost:6379';
-var pieces = uri.split(':');
 
-module.exports = function(){
-  return redis.createClient(pieces[1], pieces[0], { return_buffers: true });
-};
+var app = express();
+
+app.use(express.static(__dirname,  + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.engine('handlebars',handlebars({
+  defualtLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+    res.render('index');
+})
+
+
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('connection running')
+});
