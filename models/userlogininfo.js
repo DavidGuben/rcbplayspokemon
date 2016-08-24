9a7bfd4c536c57ef73e2fb6d460a68c90898c07c
Angelo Bovino <angelo.bovino@outlook.com>
2016-08-24 19:20:50 -0400

    restore files

--- models/userlogininfo.js
+++ models/userlogininfo.js
@@ -1,27 +1,14 @@
 'use strict';
-
-var bcrypt = require('bcrypt');
-
 module.exports = function(sequelize, DataTypes) {
   var userlogininfos = sequelize.define('userlogininfos', {
     username: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
-    freezeTableName: true,
-    instanceMethods: {
-        generateHash: function(password) {
-            return bcrypt.hash(password, bcrypt.genSaltSync(9), function(err, res) {
-              if (err) {
-                console.log(err);
-              }
-
-              console.log(res);
-            });
-        },
-        validPassword: function(password) {
-            return bcrypt.compare(password, this.password);
-        },
-    }   
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
   });
   return userlogininfos;
 };
--- redis.js
+++ redis.js
@@ -5,8 +5,6 @@ var sequelize = require('sequelize');
 var app = express();
 var path = require('path');
 var userlogininfos = require('./models')['userlogininfos'];
-var bcrypt = require('bcrypt');
-var methods = require('methods');
 
 
 app.use(bodyParser.urlencoded({
@@ -21,42 +19,6 @@ app.set('view engine', 'handlebars');
 
 
 app.use(express.static(path.join(__dirname)));
-
-function hashPassword(password) {
-  bcrypt.genSalt(11, function(err, salt) {
-    if (err) {
-      return console.log(err);
-    }
-
-    bcrypt.hash(password, salt, function(err, hash) {
-      if (err) {
-        return console.log(err);
-      }
-
-      console.log(hash);
-    });
-
-    console.log(salt);
-  });
-};
-
-/*userlogininfos.methods.hashPassword = function (password, cb) {
-  bcrypt.genSalt(11, function(err, salt) {
-    if (err) {
-      return cb(err);
-    }
-
-    bcrypt.hash(password, salt, function (err, hash) {
-      if (err) {
-        return cb(err);
-      }
-
-      return cb(null, hash);
-    })
-  })
-}*/
-
-//hashPassword('password');
 
 app.get('/', function(req, res){
  
