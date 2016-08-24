dceb124abe000e081ad0afa89ab113075a05bb1d
Angelo Bovino <angelo.bovino@outlook.com>
2016-08-24 00:36:59 -0400

    adding encryption code --not finished

--- models/userlogininfo.js
+++ models/userlogininfo.js
@@ -1,14 +1,27 @@
 'use strict';
+
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var userlogininfos = sequelize.define('userlogininfos', {
     username: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
+    freezeTableName: true,
+    instanceMethods: {
+        generateHash: function(password) {
+            return bcrypt.hash(password, bcrypt.genSaltSync(9), function(err, res) {
+              if (err) {
+                console.log(err);
+              }
+
+              console.log(res);
+            });
+        },
+        validPassword: function(password) {
+            return bcrypt.compare(password, this.password);
+        },
+    }   
   });
   return userlogininfos;
 };
